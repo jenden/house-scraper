65d66365fe9d6bbaabbffde1309bdc4d3695410e
jenden <willjenden@gmail.com>
2018-02-21 23:16:24 -0800

    Separate log file for each day

--- src/log.py
+++ src/log.py
@@ -1,8 +1,10 @@
 import logging
-
+from datetime import datetime
+import os
 logging.basicConfig(level=logging.INFO)
 
-logger = logging.getLogger('ParsingErrorLog')
-fh = logging.FileHandler('../logs/errors.log')
+logger = logging.getLogger()
+fname = datetime.now().date().isoformat() + '.log'
+fh = logging.FileHandler(os.path.join('../logs/', fname))
 fh.setFormatter(logging.Formatter(fmt='%(asctime)s %(levelname)s: %(message)s', datefmt='%H:%M:%S'))
 logger.addHandler(fh)
