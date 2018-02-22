7fe705363acce74d7d3f4bbbbcbf1527c0760a4b
jenden <willjenden@gmail.com>
2018-02-21 23:23:13 -0800

    Demonstrate unzipping compressed html

--- src/database.py
+++ src/database.py
@@ -9,21 +9,16 @@ ddb = boto3.resource('dynamodb')
 ListingTable = ddb.Table('listings')
 ExtractTable = ddb.Table('extracts')
 
-# table.put_item(Item={'ListingID':'R26','LinkDate':'2018-02-18 18:24:11','Area':'VANCOUVER-BC', 'ExtractID':123582301})
 
-# response = table.get_item(
-#     Key={
-#         'ListingID': 'R24',
-#         'LinkDate':'2018-02-18 18:24:11'
-#     }
-# )
-# print(response)
-# item = response['Item']
-# print(item)
+if __name__ == "__main__":
 
-# response = table.query(KeyConditionExpression=Key('ListingID').eq('R24'))
-# response = table.query(AttrConditionExpression=Attr('LinkDate').eq('2018-02-18 18:24:11'))
-#
-# print(response)
+    response = ExtractTable.query(KeyConditionExpression=Key('ListingID').eq('R2241950'))
+    print(response)
+    item = response['Item']
+    print(item)
 
-ExtractTable.put_item(Item={'ListingID':'R2225079', 'Table':[{'Something':'S', 'N':'Nothing'},{'More':10, 'Less':2}]})
+    # import gzip
+    # data = response['Items'][0]['original_page']['data']
+    # # data is a Binary type object with a wrapper
+    # print(type(data.value))
+    # print(gzip.decompress(data.value))
