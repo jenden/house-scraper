0f38e64981520eaec01e7e5aa30611cc4b8b71a9
jenden <willjenden@gmail.com>
2018-02-25 20:12:40 -0800

    Relative path for logging.

--- src/log.py
+++ src/log.py
@@ -3,8 +3,12 @@ from datetime import datetime
 import os
 logging.basicConfig(level=logging.INFO)
 
+LOG_DIR = os.path.join(os.path.dirname(__file__), '../logs/')
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
 logger = logging.getLogger()
 fname = datetime.now().date().isoformat() + '.log'
-fh = logging.FileHandler(os.path.join('../logs/', fname))
+fh = logging.FileHandler(os.path.join(LOG_DIR, fname))
 fh.setFormatter(logging.Formatter(fmt='%(asctime)s %(levelname)s: %(message)s', datefmt='%H:%M:%S'))
 logger.addHandler(fh)
