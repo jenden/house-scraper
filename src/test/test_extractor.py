246147b9884d8e056896c80daad1b546fc751851
jenden <willjenden@gmail.com>
2018-03-15 08:13:38 -0700

    Started unit testing

---
+++ src/test/test_extractor.py
@@ -0,0 +1,29 @@
+import unittest
+from bs4 import BeautifulSoup
+import extractors
+
+
+class ExtractorTests(unittest.TestCase):
+
+    def setUp(self):
+
+        # load page from file
+        fpath = '../../examples/R2227563.html'
+        with open(fpath, 'r') as fp:
+            page = fp.read()
+
+        self.soup = BeautifulSoup(page, 'html5lib')
+        self.mc = extractors.main_content(self.soup)
+        self.ph = extractors.property_header(self.mc)
+
+    def test_street_address(self):
+        actual = extractors.street_address(self.ph)
+        self.assertEqual(actual, '401 E 55th Avenue')
+
+    def test_listing_id_from_head(self):
+        actual = extractors.listing_id_from_head(self.soup)
+        self.assertEqual(actual, 'R2227563')
+
+
+if __name__ == '__main__':
+    unittest.main()
